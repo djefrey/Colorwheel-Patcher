db2c6a91ff6fa15e3e3ec451c07884546a684c20
djefrey <djefrey@protonmail.com>
2025-06-11 22:16:12 +0200

    port 1.21.1

---
+++ neoforge/src/main/java/dev/djefrey/neoforge/ClrwlPatcherNeoForge.java
@@ -0,0 +1,28 @@
+package dev.djefrey.neoforge;
+
+import dev.djefrey.ClrwlPatcher;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+import dev.djefrey.ExampleMod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLPaths;
+
+import java.nio.file.Path;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ClrwlPatcherNeoForge
+{
+    public ClrwlPatcherNeoForge(IEventBus modBus)
+    {
+        modBus.addListener(this::onCommonSetup);
+    }
+
+    private void onCommonSetup(FMLCommonSetupEvent event)
+    {
+        Path shaderPath = FMLPaths.GAMEDIR.get().resolve("shaderpacks");
+        Path configPath = FMLPaths.CONFIGDIR.get().resolve(ClrwlPatcher.MOD_ID);
+
+        ClrwlPatcher.init(shaderPath, configPath);
+    }
+}
