a814315e18c8cb81992dfad81433db07e8eeb86a
djefrey <djefrey@protonmail.com>
2025-06-14 20:33:08 +0200

    bump to version 0.2.1

--- common/src/main/java/dev/djefrey/ClrwlPatcher.java
+++ common/src/main/java/dev/djefrey/ClrwlPatcher.java
@@ -17,7 +17,7 @@ public final class ClrwlPatcher
     public static final String MOD_ID = "colorwheel_patcher";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
     public static final String BRAND_VERSION = "Colorwheel_" + VERSION;
 
     public static final String JAR_PATCHES_SUBPATH = "/patches/";
